c7d4c917a4368db9c034ec2a537ec3e1c64e6994
purplepaluza <dmf218@lehigh.edu>
2014-09-03 21:58:51 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+//Daniella Fodera
+//WelcomeClass Java Program 
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+// define a class
+public class WelcomeClass{
+
+    //add main method
+    public static void main(String[] args)  {
+        
+        //print the statement with the box around it
+        System.out.println("    -----------");
+        System.out.println("    | Welcome |");
+        System.out.println("    -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-D--M--F--2--1--8|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("Hi. My name is Daniella Fodera.  I'm majoring in bioengineering and I play clarinet in the Marching 97.");
+        
+        //test
+    }
+}
+                
+        
+        
+        
