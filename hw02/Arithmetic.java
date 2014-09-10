babd69fb1d90346e52d067b72a544f18026bc0cc
purplepaluza <dmf218@lehigh.edu>
2014-09-10 01:57:48 +0000

    updated Arithmetic

--- hw02/Arithmetic.java
+++ hw02/Arithmetic.java
@@ -26,35 +26,42 @@ public class Arithmetic {
         //basic calculations
         double totalSockCost$=((nSocks*sockCost$)*100)/100; //determine cost of socks before tax (bt)
         double salesTaxSocks$=(totalSockCost$*taxPercent*100)/100.0;    //determine just sales tax for socks
+        double roundOffSocksTax = Math.round(salesTaxSocks$*100)/100.00;    //round decimals
         double taxTotalSockCost$=((totalSockCost$*taxPercent+totalSockCost$)*100)/100.0;    //determine cost of socks with tax
+        double roundOffTotalSocksTax = Math.round(taxTotalSockCost$*100)/100.00;
         double totalGlassesCost$=((nGlasses*glassCost$)*100)/100;   //determine cost of glasses bt
         double salesTaxGlasses$=((totalGlassesCost$*taxPercent)*100)/100.0;   //determine just sales tax for glasses
-        double taxTotalGlassesCost$=((salesTaxGlasses$+totalGlassesCost$)*100)/100.0;   //determine cost of glasses with tax
+        double roundOffSalesTaxGlasses = Math.round(salesTaxGlasses$*100)/100.00;
+        double taxTotalGlassesCost$=(salesTaxGlasses$+totalGlassesCost$);   //determine cost of glasses with tax
+        double roundOffTaxTotalGlasses = Math.round(taxTotalGlassesCost$*100)/100.00;
         double totalEnvelopeCost$=((nEnvelopes*envelopeCost$)*100)/100; //determine cost of envelopes bt
         double salesTaxEnvelopes$=((totalEnvelopeCost$*taxPercent)*100)/100.0;    //determine sales tax of just envelopes
+        double roundOffSalesTaxEnvelopes = Math.round(salesTaxEnvelopes$*100)/100.00;   //round decimals
         double taxTotalEnvelopeCost$=((totalEnvelopeCost$*taxPercent+totalEnvelopeCost$)*100)/100.0; //determine cost of envelopes after tax
+        double roundOffTaxTotalEnvelope = Math.round(taxTotalEnvelopeCost$*100)/100.00;
         
         System.out.println("At a rate of $2.58 per pair of socks, the total cost of 3 socks before tax is $"+totalSockCost$+"."+
-            " The sales tax collected from this item is $"+salesTaxSocks$+
-            " and the total cost of 3 socks after tax is $"+taxTotalSockCost$);    //print costs of socks
+            " The sales tax collected from this item is $"+roundOffSocksTax+
+            " and the total cost of 3 socks after tax is $"+roundOffTotalSocksTax);    //print costs of socks
         System.out.println("At a rate of $2.29 per drinking glass, the total cost of 6 drinking glasses before tax is $"+
-            totalGlassesCost$+"."+" The sales tax collected from this item is $"+salesTaxGlasses$+ 
-            " and the total cost of 6 glasses after tax is $"+taxTotalGlassesCost$);  //print costs of drinking glasses
+            totalGlassesCost$+"."+" The sales tax collected from this item is $"+roundOffSalesTaxGlasses+ 
+            " and the total cost of 6 glasses after tax is $"+roundOffTaxTotalGlasses);  //print costs of drinking glasses
         System.out.println("At a rate of $3.25 per box of envelopes, the total cost of 1 box of envelopes before tax is $"+
-            totalEnvelopeCost$+"."+" The sales tax collected from this item is $"+salesTaxEnvelopes$+
+            totalEnvelopeCost$+"."+" The sales tax collected from this item is $"+roundOffSalesTaxEnvelopes+
             " and the total cost of 1 box of envelopes after tax is $"+
-            taxTotalEnvelopeCost$); //print costs of envelopes
+            roundOffTaxTotalEnvelope); //print costs of envelopes
         
         double totalPurchaseCost$=((totalSockCost$+totalGlassesCost$+totalEnvelopeCost$)*100)/100;  //determine cost of entire purchase before tax
         
         System.out.println("The total cost of the entire purchase without tax is $"+totalPurchaseCost$); //print total cost bt
         
         double totalSalesTaxCost$=((salesTaxEnvelopes$+salesTaxGlasses$+salesTaxSocks$)*100)/100;   //calculate total sales tax accrued
-        
-        System.out.println("The total sales tax collected from this purchase is $"+totalSalesTaxCost$);  //print total sales tax cost
+        double roundOff2 = Math.round(totalSalesTaxCost$*100)/100.00;   //round decimals
+        System.out.println("The total sales tax collected from this purchase is $"+roundOff2);  //print total sales tax cost
         
         double taxTotalPurchaseCost$=((taxTotalEnvelopeCost$+taxTotalGlassesCost$+taxTotalSockCost$)*100)/100;  //calculate cost of entire purchase with tax
         //print the output data
-        System.out.println("The total cost of the entire purchase with tax is $"+(taxTotalSockCost$+taxTotalGlassesCost$+taxTotalEnvelopeCost$)); 
+        double roundOff = Math.round(taxTotalPurchaseCost$*100)/100.00;
+        System.out.println("The total cost of the entire purchase with tax is $"+(roundOff)); 
     }
 }
