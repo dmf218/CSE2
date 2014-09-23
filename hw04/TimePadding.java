cdc1f1a2dc1b1c72d0a233d8781df40a92741442
purplepaluza <dmf218@lehigh.edu>
2014-09-23 04:20:28 +0000

    updated TimePadding

--- hw04/TimePadding.java
+++ hw04/TimePadding.java
@@ -20,14 +20,30 @@ public class TimePadding {
         //call scanner constructor
         Scanner myScanner;  
         myScanner= new Scanner (System.in);
-        int sec;    //establish variable
-        int min;    //establish variable
-        int hour;   //establish variable
-        int time;
+        
+        int remSec; //establish variable for remaining seconds
+        int time;   //establish variable (total seconds)
+        int hour;
+        int sec;
+        int min;
         
         System.out.print("Enter the time in seconds:"); //prompt the user to enter number of seconds
         if (myScanner.hasNextInt()) {   //accept on integer values
             time=myScanner.nextInt();   //accept user input as variable
+            hour=(time/3600);   //calculate number of hours
+            String hourPad=String.format("%02d",hour);
+            remSec=time-(3600*hour);   //calculate remaining seconds; hour seconds subtracted
+            min=(remSec/60);    //calculate number of minutes
+            String minPad=String.format("%02d",min);  
+            sec=(remSec-(60*min));    //calculate number of seconds with padded digits
+            String secPad=String.format("%02d",sec);    
+                System.out.println("The time is "+hourPad+":"+minPad+":"+secPad+".");    //print out the time
+        return; //terminate program
+        }
+        
+        else    {
+            System.out.println("You did not enter an valid number.");   //print if there is an error
+            return; //terminate program
         }
         
     }   //end of main method
